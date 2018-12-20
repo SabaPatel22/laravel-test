96b8736eb1c6cba628d184350f7a9563ff79da98
Nafis Ahmed <nafisahmedkhan9@gmail.com>
2018-12-20 11:53:03 +0530

    first commit

---
+++ resources/views/firstpage.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My First Page</title>
+</head>
+<body>
+    I am new to laravel
+</body>
+</html>
